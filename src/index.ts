4aa707b5ec8c67fff73c53cb6091e8980debc577
4rch <60948152+MisaelLG@users.noreply.github.com>
2023-12-01 11:24:37 -0400

    Ts Init

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express()
+
+app.use(express.json())
+
+const PORT = 3000
+
+app.get('/ping',(_req, res) =>{
+    
+    console.log('Funcionando');
+    res.send('pong')
+
+})
+
+app.listen(PORT, () =>{
+    console.log('Servidor Funcionando!');
+    
+})
